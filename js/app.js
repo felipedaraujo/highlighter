d4c3acf5f5ac7ff8b82c77306390a03c845beec2
Felipe de Araujo <daraujofelipe@gmail.com>
2015-09-23 16:23:46 -0300

    Add autosize

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 (function(){  
-  
-  angular.module('myApp', [])
-    .controller('myCtrl', function($scope) {
+  angular.module('myApp', ['ngRoute'])
+    .controller('myCtrl', ['$scope', '$http', function($scope, $http) {
       
-      $('textarea').highlightTextarea({
-        words: ['mel i love you <3', 'i love you too, felipedearaujo'],
-        caseSensitive: false
-      });
+      $scope.wordCategories = [
+        { key: 'adverbs', 
+          name: 'Adverbs', 
+          source: 'https://raw.githubusercontent.com/dariusk/corpora/master/data/words/adverbs.json' }, 
+        { key: 'adjs', 
+          name: 'Adjectives', 
+          source: 'https://raw.githubusercontent.com/dariusk/corpora/master/data/words/adjs.json' }
+      ];
 
-    });
+      autosize($('textarea'));
+      
+      $scope.getSpeechData = function() {
+        $http.get($scope.selectedCategory.source).success(function (data) {        
+          console.log(data[$scope.selectedCategory.key]);
+          
+          $('textarea').highlightTextarea({
+            words: data[$scope.selectedCategory.key],
+            caseSensitive: false
+          });
+        });
+      };
 
+    }]);
 })();
